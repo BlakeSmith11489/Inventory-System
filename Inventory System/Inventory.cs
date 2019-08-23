d96cbd45a98ccecf8a67a4b37ad9e6f620b215b7
Blake Smith <suicideking2525@gmail.com>
2019-08-23 08:40:37 -0500

    Add project files.

---
+++ Inventory System/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System
+{
+    class Inventory
+    {
+        public void Menu()
+        {
+            string choice = "";
+
+            while (choice != "0")
+            {
+                //Display menu
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("0, Exit");
+
+                //Get playerinput
+                choice = Console.ReadLine();
+            }
+        }
+
+        public void EquipWeapon()
+        {
+
+        }
+
+        public void UnequipWeapon()
+        {
+
+        }
+
+    }
+}
